ea70af6480fd4f42e436e1a032b367b1f6594407
Martín <magadiflo@gmail.com>
2022-09-29 23:51:24 -0500

    Componente MiniMapa

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+interface Coordenadas {
+  lng: number;
+  lat: number;
+}
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
   styles: [
+    `div {
+      width: 100%;
+      height: 350px;
+      margin: 0px;
+    }`
   ]
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
+
+  @Input() coordenadas!: Coordenadas;
+  @ViewChild('mapa') divMapa!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.coordenadas,
+      zoom: 14,
+      interactive: false,
+    });
+
+    new mapboxgl.Marker()
+      .setLngLat(this.coordenadas)
+      .addTo(mapa);
   }
 
 }
